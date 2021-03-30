34a5ee6c22f8be2c4cf1a0ba0b7f239fdb4b8891
agr1997 <g.arvind1@gmail.com>
2021-03-30 01:09:23 -0400

    Updates

---
+++ precache-manifest.f7e8e88faf8d0db137d52b140bc87000.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5294bbeb9c874daa0e7ac09ceafacd8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b2e9d6eddc74c8e9601",
+    "url": "/portfolio/static/css/3.2ad04f36.chunk.css"
+  },
+  {
+    "revision": "fffc0b863560a546ad3a",
+    "url": "/portfolio/static/css/4.fc195817.chunk.css"
+  },
+  {
+    "revision": "8a468778fdbd10cdf58b",
+    "url": "/portfolio/static/css/main.2886a4b9.chunk.css"
+  },
+  {
+    "revision": "920e1dc34a20d704956c",
+    "url": "/portfolio/static/js/2.f16efbad.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/portfolio/static/js/2.f16efbad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2e9d6eddc74c8e9601",
+    "url": "/portfolio/static/js/3.a39cc363.chunk.js"
+  },
+  {
+    "revision": "fffc0b863560a546ad3a",
+    "url": "/portfolio/static/js/4.23adb1e2.chunk.js"
+  },
+  {
+    "revision": "8a468778fdbd10cdf58b",
+    "url": "/portfolio/static/js/main.753ae2ff.chunk.js"
+  },
+  {
+    "revision": "70cb15cf4102b976b1a5",
+    "url": "/portfolio/static/js/runtime-main.9ba76ff3.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1dbd3e730f7aec982c9258e7f828327e",
+    "url": "/portfolio/static/media/cdsLogo.1dbd3e73.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "7473354fdaebc5558ba9a00562bb660f",
+    "url": "/portfolio/static/media/deeplearningai.7473354f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e9ed781f82ff29e674cef5564836bfed",
+    "url": "/portfolio/static/media/fitaLogo.e9ed781f.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "739f3ceeffdcc705f7ae395949b1eee5",
+    "url": "/portfolio/static/media/iimLogo.739f3cee.png"
+  },
+  {
+    "revision": "045546d10f0024789b3f474effbc0fab",
+    "url": "/portfolio/static/media/isenbergLogo.045546d1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "86e933bdb97bb2042fb8c949e4f93f01",
+    "url": "/portfolio/static/media/manipalLogo.86e933bd.jpg"
+  },
+  {
+    "revision": "42f39b7d3c763977c362348306d4195e",
+    "url": "/portfolio/static/media/sbdLogo.42f39b7d.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/portfolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "03f98ba91d46b63f33e3a3aa3842f375",
+    "url": "/portfolio/static/media/umassLogo.03f98ba9.png"
+  },
+  {
+    "revision": "559eb120469cd84054ee20d978485e79",
+    "url": "/portfolio/static/media/zenotiLogo.559eb120.png"
+  }
+]);
